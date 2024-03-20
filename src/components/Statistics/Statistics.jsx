4efae925b50e711504c77195935618793f60786c
i-am-SnehaChauhan <sneha2003chauhan@gmail.com>
2024-03-20 21:41:34 +0530

    Statistics added

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -3,55 +3,30 @@ import "./Statistics.css";
 
 const Statistics = () => {
     return (
-        // <div classNameName="statistics-container" >
-        //       <div classNameName="statistics-box">
-        //         <PiUser classNameName="user-icon" size={"36px"} />
-        //         <br />
-        //         <AnimatedNumber value={400} showPlus />
-        //         <span classNameName="stat-label">Students</span>
-        //       </div>
-        //       <div classNameName="statistics-box">
-        //         <PiBuildings classNameName="pi-buildings-icon" size={"36px"} />
-        //         <br />
-        //         <AnimatedNumber value={10} showPlus />
-        //         <span classNameName="stat-label">Organizations</span>
-        //       </div>
-        //       <div classNameName="statistics-box">
-        //         <HiOutlineUserGroup classNameName="mentor-icon" size={"36px"} />
-        //         <br />
-        //         <AnimatedNumber value={40} showPlus />
-        //         <span classNameName="stat-label">Mentors</span>
-        //       </div>
-        // </div>
         <div id="call-to-action" className="call-to-action">
         <div className="container aos-init aos-animate" data-aos="zoom-out">
           <div className="row">
             <div className="text-center text-lg-start">
-              <h3 className="cta-title text-center">Insights from Devcation`23</h3>
-               {/* <p classNameName="cta-text"> Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
-                anim id est laborum.</p>  */}
-
                 <div className="row counters aos-init aos-animate" data-aos="fade-up" data-aos-delay="100">
 
-                  <div className="col-lg-3 col-6 text-center">
-                    <span>4.6/5</span>
-                    <p>Hacker rating</p>
+                  <div className="flex col-lg-3 col-6 text-center ">
+                   <span>4.6/5+</span>
+                    <h4>Hacker rating</h4>
                   </div>
 
-                  <div className="col-lg-3 col-6 text-center">
-                    <span data-purecounter-start="0" data-purecounter-end="30" data-purecounter-duration="0" className="purecounter">300</span>
-                    <p>Hacks</p>
+                  <div className="flex col-lg-3 col-6 text-center">
+                    <span data-purecounter-start="0" data-purecounter-end="30" data-purecounter-duration="0" className="purecounter">300+</span>
+                    <h4>Hacks</h4>
                   </div>
 
-                  <div className="col-lg-3 col-6 text-center">
-                    <span data-purecounter-start="0" data-purecounter-end="25" data-purecounter-duration="0" className="purecounter">25</span>
-                    <p>Speakers and mentors</p>
+                  <div className="flex col-lg-3 col-6 text-center">
+                    <span data-purecounter-start="0" data-purecounter-end="25" data-purecounter-duration="0" className="purecounter">25+</span>
+                    <h4>Speakers and mentors</h4>
                   </div>
 
-                  <div className="col-lg-3 col-6 text-center">
-                    <span data-purecounter-start="0" data-purecounter-end="500" data-purecounter-duration="0" className="purecounter">500</span>
-                    <p>Hackers</p>
+                  <div className="flex col-lg-3 col-6 text-center">
+                    <span data-purecounter-start="0" data-purecounter-end="500" data-purecounter-duration="0" className="purecounter">500+</span>
+                    <h4>Hackers</h4>
                   </div>
 
                 </div>
