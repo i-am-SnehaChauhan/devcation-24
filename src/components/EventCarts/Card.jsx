739e925c3de92ea2d890e1c5d08e1e5a273168c3
i-am-SnehaChauhan <sneha2003chauhan@gmail.com>
2024-03-20 11:05:40 +0530

    Events image added

--- src/components/EventCarts/Card.jsx
+++ src/components/EventCarts/Card.jsx
@@ -48,10 +48,10 @@ function Card(props) {
                 <i className="fas fa-calendar-alt"></i>
                 <p>{props.date}</p>
             </div>
-            <div className="date-info">
+            {/* <div className="date-info">
             <i className="fas fa-map-marker-alt"></i>
                 <p>Online</p>
-            </div>
+            </div> */}
         </div>
     );
 }
