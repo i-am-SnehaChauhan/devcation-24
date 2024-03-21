469c73263d1cc5b2dacd97723e14faeb89d59f32
i-am-SnehaChauhan <sneha2003chauhan@gmail.com>
2024-03-21 20:48:04 +0530

    Analytics added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from './pages/Home'
 import Event from './pages/EventPage'
 import Team from './pages/TeamPage'
 import './App.css'
+import { Analytics } from "@vercel/analytics/react"
 
 
 function App() {
@@ -10,7 +11,9 @@ function App() {
 
   return (
     <Router>
+      <Analytics/>
       <Routes>
+        
         <Route path="/" element={<Home />} />
         <Route path="/event" element={<Event />} />
         <Route path='/team' element={<Team />} />
