9573e06a0030e4c00179b59367df52f7d4e7e009
i-am-SnehaChauhan <sneha2003chauhan@gmail.com>
2024-03-21 15:10:07 +0530

    Tracks completed

--- src/components/Tracks.jsx
+++ src/components/Tracks.jsx
@@ -3,29 +3,28 @@ import React from "react";
 const Tracks = () => {
   return (
     <>
-      <h1 id="tracks" className="Rocher-heading" style={{ color: "#2D2B46", marginBottom: "20px", marginTop: "30px"}}>
+      <h1 id="tracks" className="Rocher-heading" style={{ color: "#2D2B46", marginTop: "30px"}}>
         Tracks
       </h1>
-      <div className="flex flex-col items-center sm:items-center justify-items-center mt-5">
+      <div className="flex flex-col items-center sm:items-center justify-items-center ">
         <div className="sm:w-2/4 cursor-pointer">
-          <p className="text-#000056 text-center">
+          {/* <p className="text-#000056 text-center">
             At Devcation`24, innovation meets impact! This year, our focus is on
             addressing pressing global issues and creating positive change
             through technology.
-          </p>
+          </p> */}
           <br />
           <div>
-            Our themes include:
-            <div className="grid grid-cols-2 gap-4 mt-4">
-              <div className="flex flex-col items-center bg-blue-200 p-4 rounded-md hover:bg-blue-100 cursor-pointer">
+            <div className="grid grid-cols-2 gap-4 mt-4 ">
+              <div className="flex flex-col items-center text-white bg-gradient-to-br from-blue-900 to-blue-500 p-4 rounded-md hover:bg-blue-700 cursor-pointer">
                 <img
                   src="./Tracks&prizes/save-the-world.png"
                   alt="sustainable-development"
-                  className="w-12 h-12 mb-2"
+                  className="w-12 h-12 mb-2 "
                 />
                 Sustainable Development
               </div>
-              <div className="flex flex-col items-center bg-blue-200 p-4 rounded-md hover:bg-blue-100 cursor-pointer">
+              <div className="flex flex-col items-center text-white bg-gradient-to-br from-blue-900 to-blue-500 p-4 rounded-md hover:bg-blue-700 p-4 rounded-md hover:bg-blue-100 cursor-pointer">
                 <img
                   src="./Tracks&prizes/mental-health.png"
                   alt="mental-health"
@@ -33,7 +32,7 @@ const Tracks = () => {
                 />
                 Mental Health
               </div>
-              <div className="flex flex-col items-center bg-blue-200 p-4 rounded-md hover:bg-blue-100 cursor-pointer">
+              <div className="flex flex-col items-center text-white bg-gradient-to-br from-blue-900 to-blue-500 p-4 rounded-md hover:bg-blue-700 p-4 rounded-md hover:bg-blue-100 cursor-pointer">
                 <img
                   src="./Tracks&prizes/fist.png"
                   alt="womans-fist"
@@ -41,7 +40,7 @@ const Tracks = () => {
                 />
                 Women Empowerment
               </div>
-              <div className="flex flex-col items-center bg-blue-200 p-4 rounded-md hover:bg-blue-100 cursor-pointer">
+              <div className="flex flex-col items-center text-white bg-gradient-to-br from-blue-900 to-blue-500 p-4 rounded-md hover:bg-blue-700 p-4 rounded-md hover:bg-blue-100 cursor-pointer">
                 <img
                   src="./Tracks&prizes/innovation.png"
                   alt="innovation"
@@ -52,10 +51,10 @@ const Tracks = () => {
             </div>
           </div>
           <br />
-          <p className="mt-5">
+          {/* <p className="mt-5">
             Join us to tackle these critical topics and to make a difference
             through technology. See you at Devcation'24!
-          </p>
+          </p> */}
         </div>
       </div>
     </>
